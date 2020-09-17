5221c46b24357c4a151257becc4b6e519b13a743
intellij <irislee@caltech.edu>
2020-09-17 14:46:15 -0700

    -finished cache matrix assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## calling cacheSolve will return the inverse of a matrix.
+## The inverse will be computed and saved if its a new matrix,
+## otherwise will be called from cache if previously calculated
 
-## Write a short comment describing this function
+## Creates a cached matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() {x}
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() {m}
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Solves the inverse of a matrix if not saved
+## in cache and saves inverse in cache
+## For argument x, pass in makeCacheMatrix(a)
+## where a is the matrix to be inverted
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-}
+
+  m <- x$getinverse()
+  
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  matrix <- x$get()
+  m <- solve(matrix)
+  x$setinverse(m)
+  m
+  }
